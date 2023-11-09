e483c795e258e0ad1387f4f418bec14364b23a15
Shibli Raihan <shibli.raihan.bangli@gmail.com>
2023-11-09 20:52:29 +0600

    scop

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -11,5 +13,17 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+    public function scopeTitle(EloquentBuilder $query, string $title): EloquentBuilder
+    {
+        return $query->where("title", "like", "%" . $title . "%");
+    }
+    public function scopPopuler (EloquentBuilder $query): EloquentBuilder|QueryBuilder
+    {
+        return  $query->withCount("reviews")->orderBy('reviews_count','desc');
+    }
+    public function scopHighestRated (EloquentBuilder $query): EloquentBuilder|QueryBuilder
+    {
+        return $query->withAvg('reviews','rating')->orderBy('reviews_avg_rating','desc');
     }
 }
